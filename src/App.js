196f4c19fffc4ddc7520ef504febaf5edd81b481
devcastro <davidcastromunoz1993@gmail.com>
2021-06-16 13:46:46 -0400

    deploy fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import UserProfile from './components/UserProfile/UserProfile'
-import LinkList from './components/LinkList/LinkList'
-import Link from './components/Link/Link'
+import React from 'react';
+import UserProfile from './components/UserProfile/UserProfile';
+import LinkList from './components/LinkList/LinkList';
+import Link from './components/Link/Link';
+import './App.css'
 
 
 export default function App() {
@@ -9,7 +10,7 @@ export default function App() {
   return (
     <>
     <div className = 'bg' >
-      
+
       <UserProfile />
       <LinkList /> 
       <Link />
