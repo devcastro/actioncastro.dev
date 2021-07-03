23ea3afd792f388fd43fcb003f771091011fb1b9
devcastro <davidcastromunoz1993@gmail.com>
2021-07-03 13:21:50 -0400

    animated gradient

--- src/components/LinkList/LinkList.js
+++ src/components/LinkList/LinkList.js
@@ -1,30 +1,43 @@
-import React from 'react';
-import './LinkList.css'
+import React from "react";
+import "./LinkList.css";
 
 export default function LinkList() {
+  return (
+    <>
+      <ul className="linklist">
+        <a id="website-link" href="https://davidcastro.dev">
+          {" "}
+          <li> Portfolio </li>{" "}
+        </a>
 
+        <a id="github-link" href="https://github.com/devcastro">
+          {" "}
+          <li> GitHub </li>{" "}
+        </a>
 
+        <a id="twitch-link" href="https://www.twitch.tv/actioncastro">
+          {" "}
+          <li> Twitch </li>{" "}
+        </a>
 
+        <a id="twitter-link" href="https://twitter.com/dCastroMMA">
+          {" "}
+          <li> Twitter </li>{" "}
+        </a>
 
-    return (
-        <>  
+        <a id="instagram-link" href="https://www.instagram.com/davidcvstro/">
+          {" "}
+          <li> Instagram </li>{" "}
+        </a>
 
-            <ul className = 'linklist'>
-
-            <a id="github-link" href="https://github.com/devcastro"> <li > GitHub </li> </a>
-
-            <a id="twitch-link" href="https://www.twitch.tv/actioncastro">    <li> Twitch </li> </a> 
-                
-            <a id="twitter-link" href="https://twitter.com/dCastroMMA">   <li> Twitter </li> </a> 
-                
-            <a id="instagram-link" href="https://www.instagram.com/davidcvstro/">    <li> Instagram </li> </a>
-                
-            <a id="linkedin-link" href="https://www.linkedin.com/in/david-castro-munoz-aab526170/">    <li> LinkedIn </li> </a> 
-                
-            <a id="website-link" href="https://example.com/">    <li> Website </li> </a>
-
-            </ul>
-            
-        </>
-    )
+        <a
+          id="linkedin-link"
+          href="https://www.linkedin.com/in/david-castro-munoz-aab526170/"
+        >
+          {" "}
+          <li> LinkedIn </li>{" "}
+        </a>
+      </ul>
+    </>
+  );
 }
